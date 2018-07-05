7d80412a8221795b6737156d08be7d006fe2ea4f
Harish Nehru <harishnehru@gmail.com>
2018-07-05 11:12:26 +0100

    Add module for frontend-app

--- infrastructure-modules/frontend-app/vars.tf
+++ infrastructure-modules/frontend-app/vars.tf
@@ -1,8 +1,10 @@
 variable "min_size" {
   description = "The minimum number of EC2 Instances in the ASG"
+#  min_size = "1"
 }
 
 variable "max_size" {
+ # max_size = "2"
   description = "The maximum number of EC2 Instances in the ASG"
 }
 
